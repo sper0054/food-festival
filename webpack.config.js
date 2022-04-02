d07f9f0de2c57051609c937d379ab463a220986e
sper0054 <teresa.sperl@gmail.com>
2022-04-02 14:06:17 -0500

    bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const path = require("path");
 const  webpack  = require("webpack");
 
@@ -12,6 +13,9 @@ module.exports = {
             $: "jquery",
             jQuery: "jquery"
         }),
+        new BundleAnalyzerPlugin({
+            analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+        })
     ],
     mode: 'development'
 };
