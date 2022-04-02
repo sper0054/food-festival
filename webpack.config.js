c44e054a0c4a0729fd753adccd581dcaebe95812
sper0054 <teresa.sperl@gmail.com>
2022-04-02 10:54:52 -0500

    typo

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
     entry: './assets/js/script.js',
-    outpath: {
+    output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.bundle.js'
     },
