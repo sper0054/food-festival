c11266ef0998d9b4002541be611f84995c8827ed
sper0054 <teresa.sperl@gmail.com>
2022-04-05 13:48:11 -0500

    end of 4

--- service-worker.js
+++ service-worker.js
@@ -23,7 +23,7 @@ self.addEventListener('install', function (e) {
             return cache.addAll(FILES_TO_CACHE)
         })
     )
-});
+})
 
 self.addEventListener('activate', function (e) {
     e.waitUntil(
